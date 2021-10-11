3ac53d5b6e765f4b91013e78f009f2cd4894b26c
Viet Nguyen <n.vietqb@gmail.com>
2021-10-11 16:39:18 -0700

    Reinforcement webpack set up

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './client/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel_loader',
+                query: {
+                    presets: ['@babel/env', '@babel/react'],
+                },
+            },
+            {
+                test: /css$/,
+                exclude: /node_modules/,
+                loaders: ['style-loader', 'css-loader',]
+            },
+        ],
+    }, 
+    devServer: {
+        static: {
+          directory: path.resolve(__dirname, 'client'),
+          publicPath: '/',
+        },
+        compress: true,
+        hot: true,
+        historyApiFallback: true,
+        host: 'localhost',
+        port: 8080,
+        proxy: {
+          '/api': {
+            target: 'http://localhost:3000',
+            secure: false,
+            changeOrigin: false,
+          }
+        }
+      },
+      resolve: {
+        extensions: ['.js', '.jsx'],
+      },
+};
