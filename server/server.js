cf22baa2c191f63d4f826c3db8d21ef491cdf6f3
freyaaaaa <freyawu10@gmail.com>
2021-10-11 18:45:19 -0700

    temp

--- server/server.js
+++ server/server.js
@@ -8,6 +8,14 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, '../assests')));
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, '../dist')));
+}
+
+app.get('/', (req, res) => {
+    return res.status(200).sendFile(path.resolve(__dirname, '../client/index.html'));
+})
+
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
 
